93c23f3dae14b260a3941f3336a85b02de5317c5
ThaxK <143111369+ThaxK@users.noreply.github.com>
2023-09-28 07:48:23 -0500

    comentarios de conexion.py

--- conexion.py
+++ conexion.py
@@ -2,14 +2,16 @@ from azure.cognitiveservices.vision.customvision.training import CustomVisionTra
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
 from msrest.authentication import ApiKeyCredentials
+#importacion de clases custom vision
 
 class ConexionCustomVision:
-    # retrieve environment variables
+    # definicion del punto de conexion y la key
     ENDPOINT = "https://senacalidavidsebastian-prediction.cognitiveservices.azure.com/"
     prediction_key = "b1e81542f271448db4f31264b66a46d4"        
     project_id = "45ab08a3-bc97-4680-8496-48e561d2be37"
     model = "classifyModel"
-            
+
+    #creacion del metodo conexion
     def conn(self):        
         prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": self.prediction_key})
         predictorConn = CustomVisionPredictionClient(self.ENDPOINT, prediction_credentials)
