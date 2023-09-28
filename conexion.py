8626d8ad739be8148ef77b7b15f0d0957d003125
Atahualpa <122654156+David-Puchana@users.noreply.github.com>
2023-09-28 10:58:59 -0500

    Update conexion

--- conexion.py
+++ conexion.py
@@ -1,11 +1,12 @@
+#importacion de clases custom vision
 from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
 from msrest.authentication import ApiKeyCredentials
-#importacion de clases custom vision
 
+# diseño clase conexion servicio CustomVision
 class ConexionCustomVision:
-    # definicion del punto de conexion y la key
+    # definicion del punto de conexion, key, id del proyecto y el la iteración 
     ENDPOINT = "https://senacalidavidsebastian-prediction.cognitiveservices.azure.com/"
     prediction_key = "b1e81542f271448db4f31264b66a46d4"        
     project_id = "45ab08a3-bc97-4680-8496-48e561d2be37"
